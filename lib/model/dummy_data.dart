53d63943fc30aa3663bba24f9b1cbf05fa6ec6b2
ghigbie <gehigbie@gmail.com>
2019-06-05 20:43:22 -0600

    Add dummy data

---
+++ lib/model/dummy_data.dart
@@ -0,0 +1,13 @@
+import 'post.dart';
+
+List<Post> getPosts(){
+  return [
+    Post('iOS 12 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 13 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 14 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 15 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 16 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 17 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 18 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+  ];
+}
