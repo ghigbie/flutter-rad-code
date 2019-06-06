4fe84d3ece4b1894c84ad2d252366f0d5c28969e
ghigbie <gehigbie@gmail.com>
2019-06-05 21:15:24 -0600

    Add basic post card class

---
+++ lib/widgets/post_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PostCard extends StatelessWidget{
+  PostCard(): super();
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
