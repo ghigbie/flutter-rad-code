66fa51da9307f3d5cd482425c88308367056d8e7
ghigbie <gehigbie@gmail.com>
2019-06-05 21:03:07 -0600

    Edit data

---
+++ lib/widgets/post_lists.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PostList extends StatelessWidget{
+  PostList(): super();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
