d95eb96153ca43280909afbba3eb7d174c24b40c
ghigbie <gehigbie@gmail.com>
2019-06-05 20:09:53 -0600

    Impliment class

---
+++ lib/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text(TITLE)),
+      body: Container(
+        color: Colors.blueGrey[500]
+      )
+    );
+  }
+}
