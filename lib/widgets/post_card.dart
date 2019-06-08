86897824979a1da9ed1db23d448b1fc2e3916238
ghigbie <gehigbie@gmail.com>
2019-06-07 19:58:59 -0600

    Add new widget

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:radcode/model/post.dart';
 
+Widget _buildTags(List<String> tags){
+  List<Widget> chips = [];
+  for (var i = 0; i < tags.length; i++){
+    final chip = Container(
+      padding: EdgeInsets.only(right: 10),
+      child: Chip(
+        label: Text(tags[i]),
+        backgroundColor: Colors.blueGrey,
+      )
+    );
+    chips.add(chip);
+  }
+  return Row(children: chips);
+}
+
 Widget buildPostCard(Post post){
   return Card(
     child: Container(
@@ -9,8 +24,9 @@ Widget buildPostCard(Post post){
         crossAxisAlignment: CrossAxisAlignment.start,
         children:[
           Text(post.title, style:TextStyle(fontSize: 18)),
-          Text('Flutter really dumb text'),
-        ]
+          Text('Flutter really dumb text', style:TextStyle(fontSize: 15)),
+          _buildtags(post.tags)
+        ],
       )
       ),
     );
