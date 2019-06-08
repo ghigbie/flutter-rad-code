e3f872694591a58fcb9da909c62db184ff5fb6b5
ghigbie <gehigbie@gmail.com>
2019-06-07 20:35:50 -0600

    Import http package

--- lib/model/post.dart
+++ lib/model/post.dart
@@ -1,7 +1,16 @@
+import 'dart:convert';
+
 class Post {
   final String title;
   final String url;
   final List<String> tags;
 
   Post(this.title, this.url, this.tags); //This is a shorthand constructor
+  
+  Post.fromJSON(Map<String, dynamic> json)
+    : title = json['title'] ?? '',
+      url = json['url'] ?? '',
+      tags = json['tags'].cast<String>() ?? [];
+
+  Map<String, dynamic> toJSON() => {'title': title, 'url': url, 'tags': tags };
 }
