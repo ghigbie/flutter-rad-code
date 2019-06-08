4a98020b450d247a6528b55bdf44e018de37d892
ghigbie <gehigbie@gmail.com>
2019-06-07 21:19:00 -0600

    Change expanded to flexible type

--- lib/widgets/post_lists.dart
+++ lib/widgets/post_lists.dart
@@ -1,23 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:radcode/model/dummy_data.dart';
 import 'package:radcode/widgets/post_card.dart';
+import 'post_card.dart';
 
 class PostList extends StatelessWidget{
   PostList(): super();
-
-  final posts = getPosts();
-
+  
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView.builder(
-        padding: EdgeInsets.all(16.0),
-        itemBuilder: (BuildContext _context, int i){
-          if(i < posts.length){
-            return buildPostCard(posts[i]);
+    return FutureBuilder <List<Post>>(
+      future: Post.fetchPosts(){
+      builder: (context, snapshot){
+          if (snapshot.connectionState == ConnectonState.done){
+            return Expanded(
+              child: ListView.builder(
+                padding: EdgeInsets.all(16.0),
+                itemBuilder: (BuildContext _context, int i){
+                  if(i < posts.length){
+                    return buildPostCard(posts[i]);
+                  }
+                },
+              )
+            );
+          }else if (snapshot.hasError) {
+            throw Exception(snapshot.error);
+          }else {
+            return Container(
+              padding: EdgeInsets.all(15),
+              child: CircularProgessIndicator()
+            );
           }
         },
-      )
+      }
     );
   }
 }
