aca1faa7ebccd43803b607a4ca4dd0d72b2b8ab1
ghigbie <gehigbie@gmail.com>
2019-06-07 19:50:05 -0600

    Modify data

--- lib/model/dummy_data.dart
+++ lib/model/dummy_data.dart
@@ -2,12 +2,18 @@ import 'post.dart';
 
 List<Post> getPosts(){
   return [
-    Post('iOS 12 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
-    Post('iOS 13 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
-    Post('iOS 14 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 12 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile', 'Android', 'Vue', 'React']),
+    Post('iOS 13 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile', 'Android', 'Vue', 'React']),
+    Post('iOS 14 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile', 'Android', 'Vue', 'React']),
     Post('iOS 15 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
     Post('iOS 16 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
     Post('iOS 17 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
     Post('iOS 18 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 13 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile', 'Android', 'Vue', 'React']),
+    Post('iOS 14 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 15 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 16 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile', 'Android', 'Vue', 'React']),
+    Post('iOS 17 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 18 animation tutorial', 'https://youtube.com', ['iOS', 'Mobile', 'Android', 'Vue', 'React']),
   ];
 }
